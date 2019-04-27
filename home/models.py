16e7cc78eb5c478bf937d6a5498b8c3abf376de5
Egor14 <egorgrekhnev@mail.ru>
2019-04-27 20:05:27 +0300

    add models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Dealer(models.Model):
+    name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Car(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    model = models.CharField(max_length=50)
+    year = models.IntegerField()
+    city = models.CharField(max_length=50)
+    count = models.IntegerField()
+
+    def __str__(self):
+        return self.model
